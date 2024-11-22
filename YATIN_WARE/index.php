abefe829cb764374da16ac4950179ad3e8133ff3
harryray26 <39805544+harryray26@users.noreply.github.com>
2024-11-22 08:47:26 +0100

    Add files via upload

---
+++ YATIN_WARE/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>YATINS WARE</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="login-container">
+        <div class="login-box">
+            <div class="logo">YATIN WARE</div>
+            <h2>chris</h2>
+            <form>
+                <input type="text" placeholder="Username" required>
+                <input type="password" placeholder="Password" required>
+                <button type="submit">Login</button>
+                <a href="#" class="forgot-password">Forgot Password?</a>
+            </form>
+            <div class="signup-link">
+                Don’t have an account? <a href="#">Sign Up</a>
+            </div>
+           
+        </div>
+    </div>
+</body>
+</html>
